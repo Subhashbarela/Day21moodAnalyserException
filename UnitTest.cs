ab7bd4a00a05b8ee566d8c5379b59fa1fb3bc40d
Subhash Barela <101850283+Subhashbarela@users.noreply.github.com>
2023-03-06 18:27:00 +0530

    Update UnitTest.cs

--- UnitTest.cs
+++ UnitTest.cs
@@ -1,1 +1,21 @@
+using MoodAnlyserDay21Demo;
 
+namespace MoodAnalyserMSTestWithCore
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenSadMoodShouldReturnSad()
+        {
+            string expected = "SAD";
+            string message = "I am in Sad Mood";
+
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+
+            string mood = moodAnalyser.AnalyseMood();
+
+            Assert.AreEqual(expected, mood);
+        }
+    }
+    }
